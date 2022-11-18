261ca8745bbbefa3f24b38a1ce6417bc64986ecf
MAG080 <maggarzon@gmail.com>
2022-11-18 13:32:16 +0100

    4

--- Assets/ParticleController.cs
+++ Assets/ParticleController.cs
@@ -5,8 +5,36 @@ using UnityEngine;
 public class ParticleController : MonoBehaviour
 {
     Rigidbody rb;
+
+    public TrailRenderer[] particlesystem;
+
+    float angle;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     void Update()
     {
-        
+        angle = Input.GetAxis("Sideways");
+        float f;
+        if(angle > 0)
+        {
+            f = 1;
+        }
+        else if (angle < 0)
+        {
+            f = 1;
+        }
+        else
+        {
+            f = 0;
+
+        }
+
+        foreach(TrailRenderer p in particlesystem)
+        {
+            p.time = f;
+        }
     }
 }
