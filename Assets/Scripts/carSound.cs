96c0775cf161a46d54c7b05c2b50f91ca5722272
MAG080 <maggarzon@gmail.com>
2022-11-18 18:44:35 +0100

    7

--- Assets/Scripts/carSound.cs
+++ Assets/Scripts/carSound.cs
@@ -8,7 +8,11 @@ public class carSound : MonoBehaviour
     public float pitchfactor;
     Rigidbody rb;
 
-    float vel;
+    public Animator anim;
+
+    float vel, animd;
+    float b;
+
 
     private void Start()
     {
@@ -19,5 +23,22 @@ public class carSound : MonoBehaviour
         vel = Mathf.Round(rb.velocity.magnitude);
 
         carAudio.pitch = vel / pitchfactor;
+
+        float input = Input.GetAxis("Sideways");
+        float a;
+        if(input > 0)
+        {
+            a = 0;
+        }
+        else if (input < 0)
+        {
+            a = 1;
+        }
+        else
+        {
+            a = 0.5f;
+        }
+        b = Mathf.Lerp(b, a, 10 * Time.deltaTime);
+        anim.SetFloat("Blend", b);
     }
 }
