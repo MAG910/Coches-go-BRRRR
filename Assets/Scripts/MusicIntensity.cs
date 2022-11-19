f16f034077a35fda966cb9eeb047b04d2c057558
CarlosQui2001 <carlosqui2001@gmail.com>
2022-11-19 18:05:49 +0100

    Más música
    Arreglada transición entre menuses

--- Assets/Scripts/MusicIntensity.cs
+++ Assets/Scripts/MusicIntensity.cs
@@ -7,6 +7,10 @@ public class MusicIntensity : MonoBehaviour
 
     public float vel;
     public float intensity;
+    bool death;
+    public float effectiveDeath;
+
+    public die deathIndicator;
     public GameObject car;
     public Rigidbody rb; 
     
@@ -14,18 +18,23 @@ public class MusicIntensity : MonoBehaviour
     void Start()
     {
         rb = car.GetComponent<Rigidbody>();
+
         intensity = 1;
         vel = 30;
+        death = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         vel = rb.velocity.magnitude;
+        death = deathIndicator.ded;
+        effectiveDeath = death ? 1 : 0;
 
         var emitter = GetComponent<FMODUnity.StudioEventEmitter>();
         emitter.SetParameter("vel", vel);
         emitter.SetParameter("Intensity", intensity);
+        emitter.SetParameter("death", effectiveDeath);
 
     }
 }
