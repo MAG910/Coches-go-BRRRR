9615418e8724069ffb9f96b405c45afd43284fa9
Gmonesq <93043955+Gmonesq@users.noreply.github.com>
2022-11-22 22:03:57 +0100

    Ordenando todo y borrando lo incecesario

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject Menu;
+    public bool enabledd = false;
+
+    private void Start()
+    {
+        Menu.SetActive(false);
+        enabledd = false;
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && !enabledd)
+        {
+            enabledd = true;
+            Menu.SetActive(true);
+            Time.timeScale = 0;
+            return;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && enabledd)
+        {
+            enabledd = false;
+            Menu.SetActive(false);
+            Time.timeScale = 1;
+            return;
+        }
+    }
+
+    public void ExitToMenu()
+    {
+        enabledd = false;
+        Menu.SetActive(false);
+
+        Time.timeScale = 1;
+
+        SceneManager.LoadScene("Menu");
+
+    }
+    public void Resume()
+    {
+        enabledd = false;
+        Menu.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
