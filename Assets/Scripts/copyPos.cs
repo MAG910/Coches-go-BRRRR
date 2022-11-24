d7597dcd842c7005608e7088d9a250a5a1996c90
Gmonesq <93043955+Gmonesq@users.noreply.github.com>
2022-11-24 02:02:43 +0100

    Creando y arreglando objetos

--- Assets/Scripts/copyPos.cs
+++ Assets/Scripts/copyPos.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class copyPos : MonoBehaviour
 {
     public Transform tocpy;
+
     void Update()
     {
-        transform.position = tocpy.position;
+        transform.position = new Vector3(tocpy.position.x,50,tocpy.transform.position.z);
+        transform.localRotation.Set(90, tocpy.transform.rotation.y,0,0);
+        
+        
     }
 }
